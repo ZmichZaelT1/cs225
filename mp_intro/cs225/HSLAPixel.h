eaefd5ccfecf0680a0ebd9cfdd9977a0214e8b09
ZmichZaelT <55122684+ZmichZaelT@users.noreply.github.com>
2020-09-02 09:45:47 -0500

    finished rotation

--- mp_intro/cs225/HSLAPixel.h
+++ mp_intro/cs225/HSLAPixel.h
@@ -2,9 +2,25 @@
  * @file HSLAPixel.h
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #pragma once
 
+#include <iostream>
+#include <sstream>
+
 namespace cs225 {
+    class HSLAPixel{
+        public:
+        double h = 0;
+        double s = 0;
+        double l = 1;
+        double a = 1;
+
+        HSLAPixel();
+        HSLAPixel(double hue, double saturation, double luminance);
+        HSLAPixel(double hue, double saturation, double luminance, double alpha);
+    };
+
 }
--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -5,7 +5,20 @@
 
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  cs225::PNG *input = new cs225::PNG();
+  input->readFromFile(inputFile);
+  cs225::PNG *output = new cs225::PNG(input->width(), input->height());
+
+  for (unsigned x = 0; x < input->width(); x++) {
+    for (unsigned y = 0; y < input->height(); y++) {
+      cs225::HSLAPixel & input_pixel = input->getPixel(x, y);
+      cs225::HSLAPixel & output_pixel = output->getPixel(input->width() - 1 - x, input->height() - 1 -y);
+
+      output_pixel = input_pixel;
+    }
+  }
+
+  output->writeToFile (outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
