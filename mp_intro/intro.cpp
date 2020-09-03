809770fb9d948d50d41faa38e31c4c31c89b1802
ZmichZaelT <55122684+ZmichZaelT@users.noreply.github.com>
2020-09-03 05:29:31 -0500

    finished my art

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -2,6 +2,7 @@
 #include "cs225/HSLAPixel.h"
 
 #include <string>
+#include <cmath>
 
 
 void rotate(std::string inputFile, std::string outputFile) {
@@ -23,7 +24,29 @@ void rotate(std::string inputFile, std::string outputFile) {
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
-  // TODO: Part 3
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      cs225::HSLAPixel &pixel = png.getPixel(x, y);
+      pixel.h = 360 * (double(x) / double(width) + double(y) / double(height));
+      pixel.s = 0.7;
+      pixel.l = 0.4;
 
+      if (x >= std::floor(width * 0.25) && x <= std::floor(width * 0.75)) {
+        if ((y >= std::floor(height * 0.25) && y <= std::floor(height * 0.375)) ||
+            (y >= std::floor(height * 0.75) && y <= std::floor(height * 0.875))) {
+          pixel.h = 11 * (1.2 + double(x) / double(width) + double(y) / double (height));
+          pixel.l = 0.5;
+          pixel.s = double(x) / double(width) + 0.2;
+        }
+      }
+
+      if ((x >= std::floor(width * 0.375) && x <= std::floor(width * 0.625)) && 
+          (y >= std::floor(height * 0.375) && y <= std::floor(height * 0.875))){
+          pixel.h = 11 * (1.2 + double(x) / double(width) + double(y) / double (height));
+          pixel.l = 0.5;
+          pixel.s = double(x) / double(width) + 0.2;
+      }
+    }
+  }
   return png;
 }
