68d8f8d8a1f8eca28bc9bfbb7be3d08163ce5dd2
zz51 <zz51@gmail.com>
2020-10-17 22:08:39 -0500

    mp finished

--- mp_traversals/colorPicker/MyColorPicker.cpp
+++ mp_traversals/colorPicker/MyColorPicker.cpp
@@ -1,16 +1,22 @@
 #include "../cs225/HSLAPixel.h"
 #include "../Point.h"
-
+#include <stdlib.h>
 #include "ColorPicker.h"
 #include "MyColorPicker.h"
 
 using namespace cs225;
 
+
+MyColorPicker::MyColorPicker() {
+  hue = rand() % 360;
+}
 /**
  * Picks the color for pixel (x, y).
  * Using your own algorithm
  */
 HSLAPixel MyColorPicker::getColor(unsigned x, unsigned y) {
   /* @todo [Part 3] */
-  return HSLAPixel();
+  HSLAPixel pixel(hue, 1, 0.5);
+  hue = rand() % 360;
+  return pixel;
 }
--- mp_traversals/main.cpp
+++ mp_traversals/main.cpp
@@ -21,11 +21,25 @@ int main() {
   // - The code provided below produces the `myFloodFill.png` file you must
   //   submit Part 3 of this assignment -- uncomment it when you're ready.
   
-  /*
+  PNG png;      png.readFromFile("tests/i.png");
+
+  FloodFilledImage image(png);
+
+  MyColorPicker color1;
+  BFS bfs(png, Point(40, 40), 0.05);
+  image.addFloodFill(bfs, color1);
+
+  MyColorPicker color2;
+  DFS dfs(png, Point(40, 40), 0.05);
+  image.addFloodFill(dfs, color2);
+
+
+  Animation animation = image.animate(1000);
+
   PNG lastFrame = animation.getFrame( animation.frameCount() - 1 );
   lastFrame.writeToFile("myFloodFill.png");
   animation.write("myFloodFill.gif");
-  */
+
 
 
   return 0;
