a77b0e340f542191c9279d81ee9e952f943ba48f
zz51 <zz51@gmail.com>
2020-11-08 07:31:37 -0600

    add dummy

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -1,1 +1,28 @@
 /* Your code here! */
+#include "maze.h"
+    void SquareMaze::makeMaze (int width, int height) {
+
+    }
+
+    bool SquareMaze::canTravel (int x, int y, int dir) const {
+        return true;
+    }
+
+    void SquareMaze::setWall (int x, int y, int dir, bool exists) {
+
+    }
+
+    vector<int> SquareMaze::solveMaze () {
+        vector<int> a;
+        return a;
+    }
+
+    PNG * SquareMaze::drawMaze () const {
+        PNG *a = new PNG();
+        return a;
+    }
+
+    PNG * SquareMaze::drawMazeWithSolution () {
+        PNG *a = new PNG();
+        return a;
+    }
--- mp_mazes/maze.h
+++ mp_mazes/maze.h
@@ -1,1 +1,18 @@
 /* Your code here! */
+#pragma once
+#include "cs225/PNG.h"
+#include <vector>
+
+using namespace std;
+using namespace cs225;
+
+class SquareMaze {
+    private:
+    public:
+        void makeMaze (int width, int height);
+        bool canTravel (int x, int y, int dir) const;
+        void setWall (int x, int y, int dir, bool exists);
+        vector< int > solveMaze ();
+        PNG * drawMaze ()const; 
+        PNG * drawMazeWithSolution ();		
+};
